85deb4b13a59136dea7463d613286c3f1702308e
Rahel Weiss <mail@rahelweiss.com>
2018-03-01 11:33:17 +0800

    finish controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,27 +1,41 @@
 class ApplicationsController < ApplicationController
+  before_action :set_application, except: [:create]
 
   def create
-
+    application = Application.new(application_params)
+    if application.save
+      redirect_to job_path(application.job_id)
+    else
+    end
   end
 
   def rejected
-
+    @application.status = "rejected"
+    @application.save
   end
 
   def in_progress
-
+    @application.status = "in_progress"
+    @application.save
   end
 
   def matched
-
+    @application.status = "matched"
+    @application.save
   end
 
   def update
-
   end
 
   def destroy
-
+    @application.destroy
+    redirect_to job_path(@application.job_id)
   end
 
+  def application_params
+    params.require(:application).permit(:user_id, :job_id)
+  end
+  def set_application
+    @application = Application.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only [:show, :edit, :destroy]
+
   def index
   end
 
-  def create
+  def new
+    @user = User.new
   end
 
-  def new
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to dashboard_index_path(user)
+    else
+    end
   end
 
   def show
@@ -15,8 +23,21 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+    end
   end
 
   def destroy
+    # waiting to add the status column on user.
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :location, :resume_file_path)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
