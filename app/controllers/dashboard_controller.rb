db217fb56dec5ea106c2a212b8dd7aef88276db7
Sonia Montero <sonia.montero@skema.edu>
2018-03-02 16:35:25 +0800

    Filter by applied jobs ok on Dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @jobs = Job.all
     @favorited = current_user.favorites.map(&:job)
+    @applied = current_user.applications.map(&:job)
   end
 end
