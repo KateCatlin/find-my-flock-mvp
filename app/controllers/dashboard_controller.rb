d753cdb575e9281bb328e8fc4d87f97738196629
Sonia Montero <sonia.montero@skema.edu>
2018-03-02 15:52:34 +0800

    Make the Favorite hidden works youhouuuuuuuuuuuuu bis

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @jobs = Job.all
-    @favorited = current_user.favorites.where(job: @job).any?
+    @favorited = current_user.favorites.map(&:job)
   end
 end
