e4b96c8af9fedaa1d631d38c42cce14d174ac616
Kate Catlin <kcatlin2@gmail.com>
2018-03-05 14:31:33 +0800

    redirects upon email sign-up to user creation page

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,18 +7,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
      sign_in_and_redirect @registration, :event => :authentication
    else
      session["devise.linkedin_uid"] = request.env["omniauth.auth"]
-     redirect_to new_registration_registration_url
+     redirect_to new_user_path(@user)
    end
  end
-
- protected
-
- def after_sign_up_path_for(resource)
-  redirect_to new_user_path(@user)
-end
-
-def after_inactive_sign_up_path_for(resource)
-  redirect_to root
-end
-
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+ protected
+
+ def after_sign_up_path_for(resource)
+  new_user_path(@user)
+end
+
+def after_sign_in_path_for(resource_or_scope)
+  raise
+end
+
+def after_inactive_sign_up_path_for(resource)
+  redirect_to root
+end
 
 end
