394c4e566fcd6d3264d6158208dfca690d3241e9
Kate Catlin <kcatlin2@gmail.com>
2018-03-06 17:17:46 +0800

    LinkedIn pulling in name and other info

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,17 +7,27 @@ class Registration < ApplicationRecord
 
   def self.connect_to_linkedin(auth, signed_in_resource=nil)
     registration = Registration.where(:provider => auth.provider, :uid => auth.uid).first
-   if registration
+    if registration
+     binding.pry
      return registration
    else
      registered_registration = Registration.where(:email => auth.info.email).first
      if registered_registration
-       return registered_registration
-     else
-       registration = Registration.create(provider:auth.provider, uid:auth.uid, email:auth.info.email, password:Devise.friendly_token[0,20],
-         )
-     end
-   end
- end
-
+      binding.pry
+      return registered_registration
+    else
+      binding.pry
+      registration = Registration.create(provider:auth.provider, uid:auth.uid, email:auth.info.email, password:Devise.friendly_token[0,20])
+      image_url = auth.extra.raw_info.pictureUrl rescue nil
+      user = User.where(email: auth.info.email).first rescue nil
+      if user
+        user.update_attributes(image_url: image_url, linkedin_url: linkedin_url)
+      else
+        user = User.create(registration_id: registration.id, first_name: auth.info.first_name, last_name: auth.info.last_name, location: auth.info.location, photo: auth.info.image)
+        user.save!
+      end
+      return registration
+    end
+  end
+end
 end
