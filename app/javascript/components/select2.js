2057266d2dd28700558627480364e33f4c71d94e
Rahel Weiss <mail@rahelweiss.com>
2018-03-06 12:26:47 +0800

    select2

---
+++ app/javascript/components/select2.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import 'select2';
+
+$('.select2').select2();
+console.log('hello');
+
+// Requiring CSS! Path is relative to ./node_modules
+import 'select2/dist/css/select2.css';
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,3 @@
 import "bootstrap";
+import '../components/select2';
+
