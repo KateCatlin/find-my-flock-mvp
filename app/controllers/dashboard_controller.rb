fdb72f602c8b3a7ebc85aee1e5fe4a22b615387f
Kate Catlin <kcatlin2@gmail.com>
2018-03-07 14:52:07 +0800

    Filtering with location tags working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,8 +15,12 @@ class DashboardController < ApplicationController
 
 
   def matched_jobs
-    value_matched_jobs = Job.tagged_with(current_user.value_list, :on => :values)
-    location_matched_jobs = value_matched_jobs.tagged_with(current_user.location, :any => true)
+    if current_user.value_list.empty?
+      value_matched_jobs = Job.all
+    else
+      value_matched_jobs = Job.tagged_with(current_user.value_list, :on => :values)
+    end
+    location_matched_jobs = value_matched_jobs.tagged_with(current_user.location_list, :any => true)
     skill_matched_jobs = []
     array_of_user_skills = current_user.skill_list
     array_of_job_skills =[]
