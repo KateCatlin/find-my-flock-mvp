0f3a0f661d0f0137bd6c6f3f2be9292c28c2d584
Kate Catlin <kcatlin2@gmail.com>
2018-03-09 10:08:20 +0800

    Changing copy again

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -5,7 +5,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   self.abstract_class = true
 
-  SKILLS = %w(Java Python C Ruby Rails Javascript JQuery AngularJS Node.js React ReactNative PHP WordPress HTML CSS Objective-C Swift iOS Android Kotlin SQL .NET R Perl MATLAB Erlang Scala Bash Clojure Haskell Groovy DevOps Systems UI UX Apex SAS Crystal git GitHub Sales).sort
+  SKILLS = %w(Cybersecurity Management Architecture Mentorship AWS Java Python C Ruby Rails Javascript JQuery AngularJS Node.js React ReactNative PHP WordPress HTML CSS Objective-C Swift iOS Android Kotlin SQL .NET R Perl MATLAB Erlang Scala Bash Clojure Haskell Groovy DevOps Systems UI UX Apex SAS Crystal git GitHub Sales).sort
 
   VALUES = ["Remote", "30+ Days Parental Leave", "60+ Days Parental Leave", "90+ Days Parental Leave", "Flexible Hours", "Social Mission", "Environmental Mission", "401(k)", "401(k) Matching", "100\% Covered Health Insurance", "80\%+ Covered Health Insurance", "Dental Insurance", "Vision Insurance", "Life Insurance", "Trans-Inclusive Healthcare", "Professional Development Budget", "Unlimited Vacation", "30+ Days Vacation", "Lunch Provided", "Beach Within 60 Minutes", "Mountain Within 60 Minutes", "In-Office Gym", "Flat Heirarchy", "Commuter Coverage"].sort
 
