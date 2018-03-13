d7a519a09dad72c0fd326acc6984e993e71633c7
Kate Catlin <kcatlin2@gmail.com>
2018-03-13 15:31:49 +0100

    Rerouting from job show page if not logged in

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,11 @@
 class JobsController < ApplicationController
   def show
-    @job = Job.find(params[:id])
-    @already_applied = current_user.applications.where(job: @job).any?
-    @application = Application.find_by(user: current_user, job: @job)
+    if current_user.nil?
+      redirect_to new_registration_registration_path
+    else
+      @job = Job.find(params[:id])
+      @already_applied = current_user.applications.where(job: @job).any?
+      @application = Application.find_by(user: current_user, job: @job)
+    end
   end
 end
