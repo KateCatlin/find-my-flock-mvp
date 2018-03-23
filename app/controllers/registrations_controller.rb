4aaab5926109b8176e1fbe57d456475f9413b96f
Kate Catlin <kcatlin2@gmail.com>
2018-03-23 13:46:02 +0100

    Deleting useless initailizer and fixing pre-exhisting-email bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,13 +10,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def subscribe_to_mailchimp
-    binding.pry
     gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
     gibbon.timeout = 15
     gibbon.open_timeout = 15
     gibbon.symbolize_keys = true
     gibbon.debug = false
-    gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: resource.email, status: "subscribed"})
+    gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members(Digest::MD5.hexdigest(resource.email)).upsert(body: {email_address: resource.email, status: "subscribed"})
   end
 
   protected
--- config/initializers/gibbon.rb
+++
@@ -1,7 +0,0 @@
-require 'gibbon'
-
-gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
-gibbon.timeout = 15
-gibbon.open_timeout = 15
-gibbon.symbolize_keys = true
-gibbon.debug = false
