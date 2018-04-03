03db9216276e5072a56f11d0c5c871ff4af7a219
Benjamin Skiff <benskiff30@gmail.com>
2018-04-04 00:01:23 +0800

    should be only one card on the applications page even if user clicks it multiple times

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,5 +3,6 @@ class Application < ApplicationRecord
   belongs_to :job
 
   validates :state, presence: true, inclusion: {in: %w(pending rejected in_progress matched)}
+  validates_uniqueness_of :user_id, :scope => [:job_id]
 
 end
