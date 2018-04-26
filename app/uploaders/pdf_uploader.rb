ea2faeb19a01797564b84194fdea0ab8b9e16f3a
Sonia Montero <sonia.montero@skema.edu>
2018-04-26 15:55:02 +0200

    now accepts docx format

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -1,3 +1,12 @@
 class PdfUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  def size_range
+    1..5.megabytes
+  end
+
+   def extension_whitelist
+    %w(pdf doc html docx jpg jpeg gif png txt mp3 xls xlsx)
+  end
+
 end
