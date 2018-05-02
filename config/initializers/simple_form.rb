ab1bd0f0cf89cecef374e72ab0355de533dd1b9a
Sonia Montero <sonia.montero@skema.edu>
2018-05-02 21:37:27 +0200

    revamping of the reset password form + added a message to explain what's going to happen when entering email and submiting.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -167,5 +167,7 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
+
+  # Removes the asterisk in front of the required label fields.
   config.label_text = lambda { |label, required, explicit_label| "#{label}" }
 end
